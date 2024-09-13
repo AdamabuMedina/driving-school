12fc00fc1978e0cb99f51befa480fa4e50b033db
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-09-13 22:56:57 +0300

    Подготовка приложения и настройка Spinner.js

---
+++ src/components/Spinner.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+const Spinner = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const spinnerTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(spinnerTimeout);
+  }, []);
+
+  if (!loading) return null;
+
+  return (
+    <div
+      id="spinner"
+      className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
+    >
+      <div className="spinner-grow text-primary" role="status"></div>
+    </div>
+  );
+};
+
+export default Spinner;
